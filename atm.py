31976bb5225b3da46c6ead00bab976e38f854211
gkw314 <go980314@gmail.com>
2025-01-09 17:07:30 +0900

    🐛 입력 검증 및 에러 처리 추가
    잘못 입력된 값을 처리하도록 기능 추가, 유효하지 않은 메뉴 선택시 경고 메시지 출력

--- atm.py
+++ atm.py
@@ -8,13 +8,19 @@
 #영수증에 다음 순서로 값이 들어가도록 코드를 만들어 주세요. - > '입금', 입금 요청액, 총액 순으로 데이터 넣어주세요.
 # 단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
 #영수증 변수는 : receipts
-
+'''
+'''
 #출금한 금액을 받는 변수 : withdraw_amount
 #출금을 요청한 금액은 balance 변수에 뺀 결과가 들어가도록 코드를 작성해주세요.
 #영수증에 다음 순서로 값이 들어가도록 코드를 만들어 주세요. - > "출금", withdraw_amount, balance) 순으로 데이터 넣어주세요.
 #가지고 있는 금액보다 출금을 원하는 금액 클때 가지고 있는 금액만 출금되도록 코드를 작성해주세요.
 #단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
 #영수증 변수는 : receipts
+
+
+#입력 검증 및 에러 처리 추가
+#잘못된 입력 값(숫자가 아닌값, 음수 값 등)을 처리하도록 기능을 추가해주세요
+#유효하지 않은 메뉴 선택 시 경고 메시지 또는 사용방법 재안내를 해주세요.
 
 balance = 3000
 receipts = []
@@ -24,10 +30,13 @@ while True:
     if num == '4':
         break
     if num == '1':
-        deposit_amount = int(input("입금할 금액을 입력해주세요 : "))
-        balance += deposit_amount # balance = balance + deposit_amount
-        receipts.append(("입금", deposit_amount, balance))
-        print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.\n')
+        deposit_amount = input("입금할 금액을 입력해주세요 : ")
+        if deposit_amount.isdigit() and int(deposit_amount) > 0:
+            balance += int(deposit_amount) # balance = balance + deposit_amount
+            receipts.append(("입금", deposit_amount, balance))
+            print(f'입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.\n')
+        else:
+            print("입금한 금액을 숫자 형태와 음수가 아닌값을 입력해주세요")
 
     if num == '2':
         withdraw_amount = int(input("출금할 금액을 입력해주세요 : "))
